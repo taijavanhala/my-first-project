f7db6a72c84d0b3d26fbc1c757b7f81f8bd646a8
taijavanhala <taija@vanhala.org>
2025-09-17 11:36:12 +0300

    Add A1_T4.py

--- A1_T4.py
+++ A1_T4.py
@@ -13,18 +13,3 @@ print(f"{Num2} - {Num1} = {Diff}")
 print(f"{Sum} * {Diff} = {Product}")
 print(f"( {Num1} + {Num2} ) * ( {Num2} - {Num1} ) = {Product}")
 
-# Assign values
-Num1 = 47
-Num2 = 102
-
-# Calculations
-Sum = Num1 + Num2
-Diff = Num2 - Num1
-Product = Sum * Diff
-
-# Printing results
-print(f"{Num1} + {Num2} = {Sum}")
-print(f"{Num2} - {Num1} = {Diff}")
-print(f"{Sum} * {Diff} = {Product}")
-print(f"( {Num1} + {Num2} ) * ( {Num2} - {Num1} ) = {Product}")
-
