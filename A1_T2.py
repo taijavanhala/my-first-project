7213c202eda82f269016e2fe4101c2e4295f942a
taijavanhala <taija@vanhala.org>
2025-09-17 11:22:58 +0300

    Add A1_T2.py

---
+++ A1_T2.py
@@ -0,0 +1,18 @@
+# A1_T2.py
+
+# Assign variables
+Name1 = "John"
+Name2 = "Harry"
+
+# Print variables
+print(Name1)
+print(Name2)
+
+# Print both on same row separated by space
+print(Name1, Name2)
+
+# Print sentence using variables
+print(f"{Name1} is eating ice cream with {Name2}")
+
+# Print sentence using variables in another way
+print(f"{Name1} and {Name2} are friends")
