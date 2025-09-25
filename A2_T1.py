1a2b6876aa12830c9d4a1d2a01c4cd571e6d0bfd
taijavanhala <taija@vanhala.org>
2025-09-25 08:17:49 +0300

    Add A2_T1 basic program structure task

--- A2_T1.py
+++ A2_T1.py
@@ -1,7 +1,14 @@
-Program starting.
-What is your name: John
-Enter a floating point number: 3.1
-Enter second floating point number: 5.3
-John you gave numbers 3.1 and 5.3
-Multiplying first and second number will result in product 16.43
-Program ending.
+print("Program starting.")
+
+name = input("What is your name: ")
+num1 = float(input("Enter a floating point number: "))
+num2 = float(input("Enter second floating point number: "))
+
+product = num1 * num2
+product_rounded = round(product, 2)
+
+print(f"{name} you gave numbers {num1} and {num2}")
+print(f"Multiplying first and second number will result in product {product_rounded}")
+
+print("Program ending.")
+
