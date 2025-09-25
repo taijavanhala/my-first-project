24eab5893fda0e113e3e685e7aaaf93e4b89224e
taijavanhala <taija@vanhala.org>
2025-09-25 08:12:47 +0300

    Add A2_T1 basic program structure task

---
+++ A2_T1.py
@@ -0,0 +1,7 @@
+Program starting.
+What is your name: John
+Enter a floating point number: 3.1
+Enter second floating point number: 5.3
+John you gave numbers 3.1 and 5.3
+Multiplying first and second number will result in product 16.43
+Program ending.
