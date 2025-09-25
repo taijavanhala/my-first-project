ae9ca755eae057681e78fd673e36a82ebe101957
taijavanhala <taija@vanhala.org>
2025-09-25 09:06:16 +0300

    Add A2_T3 string length and concatenation task

---
+++ A2_T3.py
@@ -0,0 +1,12 @@
+print("Program starting.")
+
+word1 = input("Insert first word: ")
+word2 = input("Insert second word: ")
+
+print(f"1st word is {len(word1)} characters long.")
+print(f"2nd word is {len(word2)} characters long.")
+
+compound = word1 + word2
+print(f"Words together makes one closed compound '{compound}'.")
+
+print("Program ending.")
