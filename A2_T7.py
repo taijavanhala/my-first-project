052bebac91af60f34bd164913221dfd58bf11539
taijavanhala <taija@vanhala.org>
2025-09-25 09:45:34 +0300

    Add A2_T7 Fahrenheit to Celsius task

---
+++ A2_T7.py
@@ -0,0 +1,9 @@
+﻿print("Program starting.")
+
+fahrenheit = float(input("Insert fahrenheits: "))
+celsius = (fahrenheit - 32) / 1.8
+celsius = round(celsius, 1)
+
+print(f"{fahrenheit:.1f}°F is {celsius:.1f}°C")
+
+print("Program ending.")
